a779c10361fe620c455236f24e73442b4be75dce
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-10-07 20:26:42 +0200

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,115 @@
+/** @format */
+
+result = [
+	{
+		id: 1,
+        name: 'Alex'
+		guns: [
+			{
+				id: 1,
+				gun: 'Tiberius Arms T15',
+				color: 'red',
+			},
+			{
+				id: 2,
+				gun: 'Planet Eclipse Ego LV2',
+				color: 'green',
+			},
+			{
+				id: 3,
+				gun: 'HK Army VCOM Ripper',
+				color: 'yellow',
+			},
+		],
+		loaders: [
+			{
+				id: 1,
+				loader: 'Empire Prophecy Z2 Paintball Loader',
+				color: 'black',
+			},
+			{
+				id: 2,
+				loader: 'KM Rotor Dagger Spine Feed System',
+				color: 'orange',
+			},
+			{
+				id: 3,
+				loader: 'Dye LTR Paintball Loader ',
+				color: 'blue',
+			},
+		],
+	},
+	{
+		id: 2,
+        name: 'Rembo'
+		guns: [
+			{
+				id: 1,
+				gun: 'HK Army Fossil Eclipse LV2',
+			},
+			{
+				id: 2,
+				gun: 'Planet Eclipse Etha3M',
+			},
+			{
+				id: 3,
+				gun: 'DLX Luxe TM40',
+			},
+		],
+		loaders: [
+			{
+				id: 1,
+				loader: 'Empire Prophecy Z2 Paintball Loader',
+				color: 'black',
+			},
+			{
+				id: 2,
+				loader: 'KM Rotor Dagger Spine Feed System',
+				color: 'orange',
+			},
+			{
+				id: 3,
+				loader: 'Dye LTR Paintball Loader ',
+				color: 'blue',
+			},
+		],
+	},
+	{
+		id: 3,
+        name: 'John',
+		guns: [
+			{
+				id: 1,
+				gun: 'Tippmann TiPX Trufeed',
+				color: 'black',
+			},
+			{
+				id: 2,
+				gun: 'Valken V-Max Loader Max Speed Feed',
+				color: 'darkred',
+			},
+			{
+				id: 3,
+				gun: 'Tippmann Cronus Tactical',
+				color: 'blue',
+			},
+		],
+		loaders: [
+			{
+				id: 1,
+				loader: 'Empire Prophecy Z2 Paintball Loader',
+				color: 'blue',
+			},
+			{
+				id: 2,
+				loader: 'HK Army Sonic Paintball ',
+				color: 'white',
+			},
+			{
+				id: 3,
+				loader: 'Dye LTR Paintball Loader ',
+				color: 'blue',
+			},
+		],
+	},
+];
